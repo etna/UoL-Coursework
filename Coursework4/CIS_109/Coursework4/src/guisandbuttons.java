05230fb3742f35a1dc8f2e25beb5a7768f7f7958
etna <etna@localhost.(none)>
2012-03-20 20:48:01 +0800

    Started 109 CW4

---
+++ Coursework4/CIS_109/Coursework4/src/guisandbuttons.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class guisandbuttons
+{
+    public static void main (String[] args)
+    {
+        startingWindow();
+    }
+    
+    
+    
+    
+    
+    public static void startingWindow()
+    {
+        JOptionPane.showMessageDialog(null, "Welcome to my experimental Swing GUI for Coursework 4", "University of London: Java Coursework 4", 1);
+    }
+
+}
+
